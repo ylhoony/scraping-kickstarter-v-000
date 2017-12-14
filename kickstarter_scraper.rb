141d5a79bcba68fff174294c5239ad3e021f3a79
Hoon Lee <yl.hoony@gmail.com>
2017-12-14 21:09:38 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,10 +5,9 @@ require 'pry'
 def create_project_hash
   # write your code here
   html = File.read('fixtures/kickstarter.html')
-
   kickstarter = Nokogiri::HTML(html)
-
-  projects = kickstarter.css(".project")
+  lists = kickstarter.css(".project")
+  projects = Hash.new
 
   binding.pry
 
