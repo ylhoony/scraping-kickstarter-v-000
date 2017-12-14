78f9cb55e7b67220a961f9fe6ad0d9b37ea6e2f6
Hoon Lee <yl.hoony@gmail.com>
2017-12-14 21:39:51 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,6 +8,7 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
   # projects: kickstarter.css("li.project.grid_4")
   # title: project.css("h2.bbcard_name strong a").text
+  # image link: project.css("div.project-thumbnail a img").attribute("src").value
 
 
 
