f47e2ea8f51e07a7531535caaf08c068f3c3dafb
Hoon Lee <yl.hoony@gmail.com>
2017-12-14 21:47:43 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,7 +11,7 @@ def create_project_hash
   # image link: project.css("div.project-thumbnail a img").attribute("src").value
   # description: project.css("p.bbcard_blurb").text
   # location: project.css(".project-meta li a .location-name").text
-  # project.css("ul.project-stats li.first.funded strong").text
+  # project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
 
 
 
