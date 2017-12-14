879194406b352361066840682cc2fbb75042cb55
Hoon Lee <yl.hoony@gmail.com>
2017-12-14 21:28:27 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,7 +7,8 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
   # projects: kickstarter.css("li.project.grid_4")
-  
+  # title: project.css("h2.bbcard_name strong a").text
+
 
 
   # lists.each do |list|
