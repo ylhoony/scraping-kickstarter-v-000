e787d189e4b1bd44f2e0a46fbf02b15fa6beca77
Hoon Lee <yl.hoony@gmail.com>
2017-12-14 22:02:18 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -23,7 +23,6 @@ def create_project_hash
       :location => project.css("ul.project-meta span.location-name").text,
       :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
     }
-    
   end
 
   projects
